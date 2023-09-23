9e568c87da4b1d88366ca0cc9cb2f04b1bad732a
Artem Voronin <rainarlp@yandex.ru>
2023-09-23 03:46:55 +0300

    feat: organize project folder and file structure. connect to DB. Configue eslint

---
+++ app.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log('listening 3000');
+});
