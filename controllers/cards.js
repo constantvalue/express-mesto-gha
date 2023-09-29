381e99041c67604a36d30283e37f52c66234ebe9
Artem Voronin <rainarlp@yandex.ru>
2023-09-29 10:24:39 +0300

    feat: create first common error classes

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,18 +1,22 @@
 const Card = require('../models/card');
+const NotFoundError = require('../errors/NotFoundError');
+const BadRequestError = require('../errors/BadRequestError');
 
 const {
-  SERVER_FAILURE, BAD_REQUEST, NOT_FOUND, CREATED,
+  CREATED,
 } = require('../constants');
 
-module.exports.getCards = (req, res) => {
+module.exports.getCards = (req, res, next) => {
   Card.find({})
   // res.send по умолчанию имеет status(200)
     .then((cards) => res.send({ data: cards }))
-    // ловим ошибки если сервер пятисотит
-    .catch(() => res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' }));
+    .catch((err) => {
+      next(err);
+    });
+  // ловим ошибки если сервер пятисотит
 };
 
-module.exports.addCard = (req, res) => {
+module.exports.addCard = (req, res, next) => {
   const { name, link } = req.body;
   const likes = [];
   Card.create({
@@ -22,65 +26,65 @@ module.exports.addCard = (req, res) => {
     .catch((err) => {
       // ValidationError  -  это имя ошибки. Получил её с помощью console.log
       if (err.name === 'ValidationError') {
-        return res.status(BAD_REQUEST).send({ message: 'Некорректный запрос' });
+        next(new BadRequestError('Некорректный запрос'));
       }
 
-      return res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' });
+      next(err);
     });
 };
 
-module.exports.deleteCard = (req, res) => {
+module.exports.deleteCard = (req, res, next) => {
   Card.findByIdAndDelete(req.params.cardId)
     .then((card) => {
       if (!card) {
-        res.status(NOT_FOUND).send({ message: 'Нет карточки с таким id' });
+        throw new NotFoundError('Нет карточки с таким id');
       } else {
         res.send(card);
       }
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(BAD_REQUEST).send({ message: 'Некорректный запрос' });
+        next(new BadRequestError('Некорректный запрос'));
       }
 
-      return res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' });
+      next(err);
     });
 };
 
-module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
+module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
   { new: true },
 )
   .then((card) => {
     if (!card) {
-      return res.status(NOT_FOUND).send({ message: 'Нет карточки с таким id' });
+      throw new NotFoundError('Нет карточки с таким id');
     }
     return res.send(card);
   })
   .catch((err) => {
     if (err.name === 'CastError') {
-      return res.status(BAD_REQUEST).send({ message: 'Некорректный запрос' });
+      next(new BadRequestError('Некорректный запрос'));
     }
 
-    return res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' });
+    next(err);
   });
 
-module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
+module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,
   { $pull: { likes: req.user._id } }, // убрать _id из массива
   { new: true },
 )
   .then((card) => {
     if (!card) {
-      return res.status(NOT_FOUND).send({ message: 'Нет карточки с таким id' });
+      throw new NotFoundError('Нет карточки с таким id');
     }
     return res.send(card);
   })
   .catch((err) => {
     if (err.name === 'CastError') {
-      return res.status(BAD_REQUEST).send({ message: 'Некорректный запрос' });
+      next(new BadRequestError('Некорректный запрос'));
     }
 
-    return res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' });
+    next(err);
   });
--- controllers/users.js
+++ controllers/users.js
@@ -3,28 +3,28 @@ const NotFoundError = require('../errors/NotFoundError');
 const BadRequestError = require('../errors/BadRequestError');
 
 const {
-  SERVER_FAILURE, BAD_REQUEST, NOT_FOUND, CREATED,
+  CREATED,
 } = require('../constants');
 
-module.exports.getUsers = (req, res) => {
+module.exports.getUsers = (req, res, next) => {
   User.find({})
   // res.send по умолчанию имеет status(200)
     .then((users) => res.send({ data: users }))
     // ловим ошибки если сервер пятисотит
-    .catch(() => res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' }));
+    .catch(next);
 };
 
-module.exports.addUser = (req, res) => {
+module.exports.addUser = (req, res, next) => {
   const { name, about, avatar } = req.body;
   User.create({ name, about, avatar })
     .then((user) => res.status(CREATED).send(user))
     .catch((err) => {
       // ValidationError  -  это имя ошибки. Получил её с помощью console.log
       if (err.name === 'ValidationError') {
-        return res.status(BAD_REQUEST).send({ message: 'Некорректный запрос' });
+        next(new BadRequestError('Некорректный запрос'));
       }
 
-      return res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' });
+      next(err);
     });
 };
 
@@ -37,52 +37,50 @@ module.exports.getUserById = (req, res, next) => {
       } else {
         res.send({ data: user });
       }
-      // send по дефолту имеет statuscode 200
     })
     .catch((err) => {
-
       if (err.name === 'CastError') {
-        throw new BadRequestError('Некорректный запрос');
+        next(new BadRequestError('Некорректный запрос'));
       }
       next(err);
     });
 };
 
-module.exports.updateAvatar = (req, res) => {
+module.exports.updateAvatar = (req, res, next) => {
   User.findByIdAndUpdate(req.user._id, {
     avatar: req.body.avatar,
   }, { new: true, runValidators: true }).then((user) => {
     if (!user) {
-      res.status(NOT_FOUND).send({ message: 'Пользователь с таким Id не найден' });
+      throw new NotFoundError('Нет пользователя с таким id');
     } else {
       res.send({ data: user });
     }
   })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(BAD_REQUEST).send({ message: 'Некорректный запрос' });
+        next(new BadRequestError('Некорректный запрос'));
       }
-      return res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' });
+      next(err);
     });
 };
 
-module.exports.updateProfile = (req, res) => {
+module.exports.updateProfile = (req, res, next) => {
   User.findByIdAndUpdate(req.user._id, {
     name: req.body.name,
     about: req.body.about,
   }, { new: true, runValidators: true })
     .then((user) => {
       if (!user) {
-        return res.status(NOT_FOUND).send({ message: 'Пользователь с таким Id не найден' });
+        throw new NotFoundError('Нет пользователя с таким id');
       }
 
       return res.send({ data: user });
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return res.status(BAD_REQUEST).send({ message: 'Некорректный запрос' });
+        next(new BadRequestError('Некорректный запрос'));
       }
 
-      return res.status(SERVER_FAILURE).send({ message: 'Произошла ошибка' });
+      next(err);
     });
 };
