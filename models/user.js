83127ef89a5c3fd92d041c2918c9ba830b825da7
Artem Voronin <rainarlp@yandex.ru>
2023-09-29 11:01:56 +0300

    feat: refactor user model. Add regex for avatar URL validation

--- models/user.js
+++ models/user.js
@@ -1,23 +1,45 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
 
   name: {
     type: String,
-    required: true,
+    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
   },
 
   about: {
     type: String,
-    required: true,
+    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
   },
 
   avatar: {
     type: String,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: {
+      // https://uibakery.io/regex-library/url
+      validator: (v) => /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/.test(v),
+
+      message: 'Это не ссылка',
+    },
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: {
+      validator: (v) => validator.isEmail(v),
+      message: 'Это не email',
+    },
+  },
+
+  password: {
+    type: String,
+    select: false,
     required: true,
   },
 
